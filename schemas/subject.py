93c6769b7d75d1f25fac77034a78c7e7a6a79523
Smarika <smarikakc96@gmail.com>
2026-05-13 10:25:17 +0545

    feat: admin can create and assign subjects to teachers

---
+++ schemas/subject.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel
+
+class SubjectSchema(BaseModel):
+    name: str
+    credit_hr: int
+    faculty: str
+    semester: int
+    teacher_id: int
+
+class SubjectResponse(BaseModel):
+    id: int
+    name: str
+    credit_hr: int
+    faculty: str
+    semester: int
+    teacher_id: int
+
+    class Config:
+        from_attributes = True
