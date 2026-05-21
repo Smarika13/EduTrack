13d4441d673d8d8a53866fbcdf679b3364c39611
Smarika <smarikakc96@gmail.com>
2026-05-21 11:53:02 +0545

    style: fix flake8 formatting errors

--- caching.py
+++ caching.py
@@ -4,9 +4,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-REDIS_URL=os.getenv("REDIS_URL")
+REDIS_URL = os.getenv("REDIS_URL")
 
 caching = redis.from_url(REDIS_URL)
+
 
 def get_redis():
     return caching
